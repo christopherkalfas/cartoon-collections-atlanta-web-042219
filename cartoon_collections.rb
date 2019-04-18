e94f256322cb89bacc5c36e53ccc20f89a954b38
Christopher Kalfas <chriskalfas@yahoo.com>
2019-04-18 16:45:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(calls)# code an argument here
 end 
 
 def long_planeteer_calls(calls)
-  calls.any? {|call| call.length >=3}
+  calls.all? {|call| call.length > 4}
 end
 
 def find_the_cheese# code an argument here
